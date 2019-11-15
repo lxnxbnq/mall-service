053ec8b72abe1b65a0811c35169787c2c25d6b6c
wb-zqy582530 <wb-zqy582530@alibaba-inc.com>
2019-11-15 11:43:19 +0800

    lint: remove tslint & add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "no-var": 1,
+        'no-console': 1,
+        'consistent-return': 1,
+        'default-case': 1,
+        // note you must disable the base rule as it can report incorrect errors
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": ["error", {
+          "vars": "all",
+          "args": "after-used",
+          "ignoreRestSiblings": false
+        }],
+    }
+};
