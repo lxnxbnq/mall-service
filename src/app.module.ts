890e44dd78308b98acf650e0ab27ea9ab0eba6ea
wb-zqy582530 <wb-zqy582530@alibaba-inc.com>
2019-11-15 11:44:42 +0800

    update: update main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatController } from './cat.controller';
+import { CatService } from './cat.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, CatController],
+  providers: [AppService, CatService],
 })
 export class AppModule {}
